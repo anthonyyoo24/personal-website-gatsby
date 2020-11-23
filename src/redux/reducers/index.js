3e0fbbc2c221613efe599ff2bc22d665f263bf91
anthonyyoo91 <anthony_yoo_24@hotmail.com>
2020-11-22 21:47:29 -0500

    Add post reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux"
 import headerReducer from "./headerReducer"
+import postReducer from "./postReducer"
 
 export default combineReducers({
+  posts: postReducer,
   headerStyle: headerReducer,
 })
