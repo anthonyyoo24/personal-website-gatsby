9658662fbe72a7c919dff08be113cc25b1406947
anthonyyoo91 <anthony_yoo_24@hotmail.com>
2020-11-23 21:08:20 -0500

    Add form reducer from redux-form

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux"
+import { reducer as formReducer } from "redux-form"
 import headerReducer from "./headerReducer"
 import postReducer from "./postReducer"
 
 export default combineReducers({
   posts: postReducer,
+  form: formReducer,
   headerStyle: headerReducer,
 })
