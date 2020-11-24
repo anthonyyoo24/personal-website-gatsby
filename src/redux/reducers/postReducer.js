333a8e9cfa10bc0059630174a1b047f86ecda2e8
anthonyyoo91 <anthony_yoo_24@hotmail.com>
2020-11-24 10:02:26 -0500

    Include a fetch post case and store action payloads for the fetch posts case in the local storage

--- src/redux/reducers/postReducer.js
+++ src/redux/reducers/postReducer.js
@@ -1,9 +1,21 @@
 import _ from "lodash"
 
-const postReducer = (state = {}, action) => {
+const INITIAL_STATE =
+  typeof localStorage !== "undefined"
+    ? JSON.parse(localStorage.getItem("posts"))
+    : {}
+
+const postReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case "FETCH_POSTS":
+      localStorage.setItem(
+        "posts",
+        JSON.stringify({ ...state, ..._.mapKeys(action.payload, "slug") })
+      )
+
       return { ...state, ..._.mapKeys(action.payload, "slug") }
+    case "FETCH_POST":
+      return { ...state, [action.payload.slug]: action.payload }
     default:
       return state
   }
