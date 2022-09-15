0325906a360f8497886ddbcdc746c5af8c57a908
anthonyyoo91 <anthony_yoo_24@hotmail.com>
2022-09-15 05:51:37 -0400

    blah

--- src/components/StoryText.js
+++ src/components/StoryText.js
@@ -4,12 +4,12 @@ const StoryText = () => {
   return (
     <React.Fragment>
       <p>
-        Hi! I am a self-taught developer from Waterloo, Canada with a passion for problem solving,
-        building, and designing. Since the first day I started coding, I was hooked and realized
-        this was what I wanted to dedicate my career towards. Learning new technologies is always
-        exciting and something I look forward to as I continue to develop my skills. And I am
-        excited by the prospect of working on amazing products that will continue to enrich peoples’
-        lives long after they are built. So I would love to bring my passion, drive, and skills to a
+        Hi! I am a self-taught developer with a passion for problem solving, building, and
+        designing. Since the first day I started coding, I was hooked and realized this was what I
+        wanted to dedicate my career towards. Learning new technologies is always exciting and
+        something I look forward to as I continue to develop my skills. And I am excited by the
+        prospect of working on amazing products that will continue to enrich peoples’ lives long
+        after they are built. So I would love to bring my passion, drive, and skills to a
         like-minded team where we can help each other grow and make a positive difference in the
         world.
       </p>
